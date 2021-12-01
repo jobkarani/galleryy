a3d5979e4ff279db505800cfea6dbdc4d72ae3e5
Job Karani <moringa@moringa-Thinkpad-L450>
2021-12-01 06:11:07 +0300

    sets up cloudinary

--- galleryy/settings.py
+++ galleryy/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
